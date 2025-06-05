297ca92b8ad348cefc177a43042c906a7c206dc4
Glng01rmdhn <galang2909@student.abudzar.sch.id>
2025-06-05 11:35:43 +0800

    sas selesai

--- ujian-akhir/src/SAS.jsx
+++ ujian-akhir/src/SAS.jsx
@@ -5,7 +5,7 @@ function SAS() {
     return (
         <div>
             <h1>SAS</h1>
-            <p>Ini adalah foto saya ketika saya sedang di kawah putri.</p>
+            <p>Ini adalah foto saya ketika saya sedang di kawah ratu di ketinggian 1437 MDPL.   </p>
             <img style={{width: "200px"}} src={galang} alt="foto galang"/>
         </div>
     );
